08ddd14a2fb6f662adece76901a61a18f5eaab41
juliankraft <kraftjulian@gmail.com>
2025-04-02 13:57:12 +0200

    install dev packages to env mega

---
+++ code/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='BA',
+    version='0.0.0',
+    description='',
+    author='Julian Kraft',
+    author_email='kraftjul@students.zhaw.ch',
+    url='https://github.com/juliankraft/BachelorThesis',
+    packages=find_packages(),
+)
