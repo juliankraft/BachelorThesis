fef76d2688407660c2c9672e2732750e1d4dbd34
juliankraft <kraftjulian@gmail.com>
2025-04-07 00:33:58 +0200

    image pipline started

--- code/src/dataloader.py
+++ code/src/dataloader.py
@@ -17,6 +17,89 @@ from sklearn.model_selection import train_test_split
 from ba_dev.runner import MegaDetectorRunner
 
 
+class ImagePipeline:
+    def __init__(
+            self,
+            path: str | PathLike,
+            ):
+
+        self.img: Image.Image = Image.open(path)
+
+    def load(
+            self,
+            path: str | Path
+            ):
+        self.img = Image.open(path)
+        return self
+
+    def to_rgb(self):
+        self.img = self.img.convert("RGB")
+        return self
+
+    def crop_by_bbox(
+            self,
+            bbox: list[float] | tuple[float, float, float, float]
+            ):
+
+        width, height = self.img.size
+
+        x1 = int(bbox[0] * width)
+        y1 = int(bbox[1] * height)
+        x2 = int((bbox[0] + bbox[2]) * width)
+        y2 = int((bbox[1] + bbox[3]) * height)
+
+        self.img = self.img.crop((x1, y1, x2, y2))
+        self.img = self.img.crop((x1, y1, x2, y2))
+
+    def crop_center_sample(
+            self,
+            bbox: list[float] | tuple[float, float, float, float],
+            sample_size: int | Sequence[int] = (50, 50)
+            ):
+
+        if isinstance(sample_size, int):
+            target_width = target_height = sample_size
+        elif isinstance(sample_size, Sequence):
+            if len(sample_size) == 1:
+                target_width = target_height = sample_size[0]
+            elif len(sample_size) == 2:
+                target_width, target_height = sample_size
+            else:
+                raise ValueError("sample_size must be an int or a sequence of maximum two integers.")
+
+        width, height = self.img.size
+
+        center_x = int(bbox[0] * width) + int(bbox[2] * width / 2)
+        center_y = int(bbox[1] * height) + int(bbox[3] * height / 2)
+
+        x1 = center_x - (target_width // 2)
+        y1 = center_y - (target_height // 2)
+        x2 = x1 + target_width
+        y2 = y1 + target_height
+
+        self.img = self.img.crop((x1, y1, x2, y2))
+        return self
+
+    def resize(
+            self,
+            size: Sequence[int] | int,
+            ):
+        if isinstance(size, int):
+            size = (size, size)
+        elif isinstance(size, Sequence):
+            if len(size) == 1:
+                size = (size[0], size[0])
+            elif len(size) == 2:
+                size = (size[0], size[1])
+            else:
+                raise ValueError("size must be an int or a sequence of maximum two integers.")
+        self.img = self.img.resize(size)
+        return self
+
+    def get(self) -> Image.Image:
+        return self.img
+
+
 class MammaliaData(Dataset):
     """
     A class to load and process the Mammalia dataset. It can be used for the initial detection of the images
@@ -347,21 +430,6 @@ class MammaliaData(Dataset):
 
         return bb_list
 
-    def crop_by_bbox(
-            self,
-            image: Image.Image,
-            bbox: list[float] | tuple[float, float, float, float]
-            ) -> Image.Image:
-
-        width, height = image.size
-
-        x1 = int(bbox[0] * width)
-        y1 = int(bbox[1] * height)
-        x2 = int((bbox[0] + bbox[2]) * width)
-        y2 = int((bbox[1] + bbox[3]) * height)
-
-        return image.crop((x1, y1, x2, y2))
-
     def __len__(self) -> int:
         return len(self.ds)
 
@@ -431,35 +499,6 @@ class MammaliaDataImage(MammaliaData):
                 out_rows.append(new_row)
 
         return pd.DataFrame(out_rows)
-
-    def crop_center_sample(
-            self,
-            image: Image.Image,
-            bbox: list[float] | tuple[float, float, float, float],
-            sample_size: int | Sequence[int] = (50, 50)
-            ) -> Image.Image:
-
-        if isinstance(sample_size, int):
-            target_width = target_height = sample_size
-        elif isinstance(sample_size, Sequence):
-            if len(sample_size) == 1:
-                target_width = target_height = sample_size[0]
-            elif len(sample_size) == 2:
-                target_width, target_height = sample_size
-            else:
-                raise ValueError("sample_size must be an int or a sequence of maximum two integers.")
-
-        width, height = image.size
-
-        center_x = int(bbox[0] * width) + int(bbox[2] * width / 2)
-        center_y = int(bbox[1] * height) + int(bbox[3] * height / 2)
-
-        x1 = center_x - (target_width // 2)
-        y1 = center_y - (target_height // 2)
-        x2 = x1 + target_width
-        y2 = y1 + target_height
-
-        return image.crop((x1, y1, x2, y2))
 
     def __len__(self):
         pass
