bc115fd8171dd6daa20e95e19d5416bbb9d982e4
juliankraft <kraftjulian@gmail.com>
2025-04-13 15:43:50 +0200

    added best_weighted_split

--- code/src/utils.py
+++ code/src/utils.py
@@ -1,4 +1,5 @@
 import yaml
+import numpy as np
 from pathlib import Path
 
 
@@ -6,3 +7,30 @@ def load_config_yaml(path_to_config):
     with open(path_to_config, 'r') as f:
         path_config = yaml.safe_load(f)
     return {k: Path(v) for k, v in path_config.items()}
+
+
+def best_weighted_split(
+        weights: np.ndarray,
+        target_left_sum: float
+        ):
+
+    total_sum = weights.sum()
+    target_right_sum = total_sum - target_left_sum
+
+    best_cut_idx = None
+    best_error = float('inf')
+
+    for idx in range(1, len(weights)):
+        left = weights[:idx].sum()
+        right = total_sum - left
+
+        error = abs(left - target_left_sum) + abs(right - target_right_sum)
+
+        if error < best_error:
+            best_error = error
+            best_cut_idx = idx
+
+    if best_cut_idx is None:
+        raise ValueError("No valid cut found")
+
+    return best_cut_idx
