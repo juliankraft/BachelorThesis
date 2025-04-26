097890659100767f11bc47d8932ba3d5187b08c8
juliankraft <kraftjulian@gmail.com>
2025-04-26 22:50:29 +0200

    build: trainer ready for som testing

---
+++ code/src/trainer.py
@@ -0,0 +1,57 @@
+import pytorch_lightning as L
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, LearningRateMonitor
+from pytorch_lightning.loggers import TensorBoardLogger, CSVLogger
+from typing import Literal, Any
+from pathlib import Path
+
+
+class MammaliaTrainer(L.Trainer):
+    def __init__(
+            self,
+            log_dir: Path,
+            accelerator: Literal['cpu', 'gpu'] = 'cpu',
+            devices: int = 1,
+            patience: int = 5,
+            log_every_n_steps: int = 1,
+            trainer_kwargs: dict[str, Any] = {}
+            ):
+
+        tb_logger = TensorBoardLogger(
+            save_dir=log_dir,
+            name=None,
+            version=None
+            )
+
+        csv_logger = CSVLogger(
+            save_dir=log_dir,
+            name=None,
+            version=None
+            )
+
+        loggers = [tb_logger, csv_logger]
+
+        callbacks = [
+            ModelCheckpoint(
+                filename='best',
+                save_last=True,
+                save_top_k=1,
+                every_n_epochs=1
+                ),
+            EarlyStopping(
+                monitor='valid_loss',
+                patience=patience,
+                ),
+            LearningRateMonitor(
+                logging_interval='epoch'
+                )
+            ]
+
+        super().__init__(
+            default_root_dir=log_dir,
+            logger=loggers,
+            callbacks=callbacks,
+            accelerator=accelerator,
+            devices=devices,
+            log_every_n_steps=log_every_n_steps,
+            **trainer_kwargs
+            )
