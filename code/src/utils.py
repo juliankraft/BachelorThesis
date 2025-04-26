0496ff6b4e89a8a08dcfd8b27b2d74b037b896fc
juliankraft <kraftjulian@gmail.com>
2025-04-27 00:26:39 +0200

    build: prediction_writer

--- code/src/utils.py
+++ code/src/utils.py
@@ -1,11 +1,15 @@
+import csv
+import json
 import yaml
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import pytorch_lightning as L
 from pathlib import Path
 from PIL import Image
-from typing import Sequence
+from typing import Sequence, Any
+from os import PathLike
 from matplotlib.figure import Figure
 from matplotlib.axes import Axes
 
@@ -105,7 +109,7 @@ def draw_bbox_on_ax(
     Args:
         ax (Axes): The axes to draw into.
         image (Image.Image): The image to plot.
-        bbox (BBox): Bounding box [x, y, w, h] in relative coords (0–1).
+        bbox (BBox): Bounding box [x, y, w, h] in relative coords (0-1).
         conf (float, optional): Confidence score to annotate.
     """
     width, height = image.size
@@ -132,3 +136,83 @@ def draw_bbox_on_ax(
             clip_on=False
         )
     ax.axis('off')
+
+
+class PredictionWriter(L.Callback):
+    def __init__(
+            self,
+            output_path: PathLike
+            ):
+        super().__init__()
+
+        self.output_path = Path(output_path)
+        self.output_file = self.output_path / 'predictions.csv'
+        self._csv_file = None
+        self._writer = None
+        self._header_written = False
+
+    def on_predict_start(
+            self, trainer: L.Trainer, 
+            pl_module: L.LightningModule
+            ):
+        self.output_path.mkdir(parents=True, exist_ok=True)
+        self._csv = open(self.output_file, "w", newline="", encoding="utf-8")
+        self._writer = None
+        self._header_written = False
+
+    def on_predict_batch_end(
+            self,
+            trainer: L.Trainer,
+            pl_module: L.LightningModule,
+            outputs: list[dict[str, Any]],
+            batch: dict[str, Any],
+            batch_idx: int,
+            dataloader_idx: int
+            ):
+
+        if isinstance(outputs, dict):
+            outputs_list = [outputs]
+        else:
+            outputs_list = outputs
+
+        for out_dict in outputs_list:
+            B = len(next(iter(out_dict.values())))
+
+            for i in range(B):
+                row = {}
+                for k, v in out_dict.items():
+                    elem = v[i] if hasattr(v, "__len__") else v
+                    row[k] = self._sanitize(elem)
+
+                if not self._header_written:
+                    self._writer = csv.DictWriter(self._csv, fieldnames=list(row.keys()))
+                    self._writer.writeheader()
+                    self._header_written = True
+
+                assert self._writer is not None
+                self._writer.writerow(row)
+
+    def on_predict_end(
+            self,
+            trainer: L.Trainer,
+            pl_module: L.LightningModule
+            ):
+        if self._csv:
+            self._csv.close()
+
+    @staticmethod
+    def _sanitize(x):
+
+        if isinstance(x, torch.Tensor):
+            x = x.detach().cpu().tolist()
+
+        if hasattr(x, "tolist") and not isinstance(x, (str, bytes, dict, list)):
+            try:
+                x = x.tolist()
+            except Exception:
+                pass
+
+        if isinstance(x, (list, dict)):
+            return json.dumps(x, ensure_ascii=False)
+
+        return x
