5bb79bcb05bb23066d8fb24c2e69ac5bfed6ee21
juliankraft <kraftjulian@gmail.com>
2025-06-10 09:26:54 +0200

    test

--- code/src/eval_helpers.py
+++ code/src/eval_helpers.py
@@ -20,8 +20,6 @@ from typing import Dict, Any
 from ba_dev.dataset import MammaliaDataImage
 from ba_dev.utils import BBox
 
-a = 5
-
 
 def set_custom_plot_style():
     plt.rcParams['axes.titlesize'] = 10
