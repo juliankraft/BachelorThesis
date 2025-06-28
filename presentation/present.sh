de75782d3d89883234b273fadaa8ea37fcf2026a
juliankraft <kraftjulian@gmail.com>
2025-06-28 12:38:34 +0200

    build: presentation

---
+++ presentation/present.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Variables
+TARGET_FOLDER=../out/
+SOURCE_IMAGES=./images
+LOGO_IMAGE=logo.jpg
+
+# Clear the target folder if it exists
+if [ -d "$TARGET_FOLDER" ]; then
+    rm -rf "$TARGET_FOLDER"
+    echo "Cleared target folder: $TARGET_FOLDER"
+else
+    echo "Target folder does not exist. No need to clear."
+fi
+
+# Export the presentation
+reveal-md presentation.md --css simple_jk.css --static "$TARGET_FOLDER"
+if [ $? -eq 0 ]; then
+    echo "Presentation exported successfully to $TARGET_FOLDER!"
+else
+    echo "Error: Failed to export presentation." >&2
+    exit 1
+fi
+
+# Create images folder if it doesn't exist
+mkdir -p "$TARGET_FOLDER/images"
+
+# Copy all images to the static folder
+if [ -d "$SOURCE_IMAGES" ]; then
+    cp -r "$SOURCE_IMAGES/"* "$TARGET_FOLDER/images/"
+    echo "All images copied to $TARGET_FOLDER/images/"
+else
+    echo "Error: Source image directory ($SOURCE_IMAGES) not found." >&2
+    exit 1
+fi
+
+echo "Script completed successfully!"
---
+++ presentation/presentation.md
@@ -0,0 +1,45 @@
+---
+css: "simple_jk.css"
+transition: "slide"
+highlightTheme: "github"
+slideNumber: false
+title: "Presentation Bachelors Thesis"
+---
+
+## Präsentation der Bachelor Arbeit
+
+---
+
+## Inhalt
+
+* Präsentation der Arbeit
+    - Themenvorgabe
+    - Ziele
+    - Methoden
+    - Resultate
+    - Ausblick
+* Diskussion
+* Fazit
+
+Note:
+kurz den Inhalt vorstellen
+
+---
+
+## Themenvorgabe
+
+----
+
+Darauf habe ich mich beworben:
+
+<img src="images/fotofalle_rehwild.jpg" style="width: 50%; margin-bottom: -50px;">
+
+<span style="font-size: 0.4em;">*Abbildung: Vom Bayerischen Landesanstalt für Wald und Forstwirtschaft*</span>
+
+"Tiere in Fotofallendataset mit KI autmatisch erkennen"
+
+----
+
+Darum ging es tatsächlich:
+
+"Deep Learning for Biodiversity Monitoring: Automated Classification of Small Mammals Captured in Foto Trap Boxes"
